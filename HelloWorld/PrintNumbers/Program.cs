b5d485165d6304cc5addf42cb1552a355260531f
Nikola Stoykov <nikoalaaa@gmail.com>
2016-10-16 23:08:27 +0300

    Fix Print Numbers Task

--- HelloWorld/PrintNumbers/Program.cs
+++ HelloWorld/PrintNumbers/Program.cs
@@ -11,7 +11,7 @@ namespace PrintNumbers
         static void Main(string[] args)
         {
             Console.WriteLine(1);
-            Console.WriteLine(11);
+            Console.WriteLine(101);
             Console.WriteLine(1001);
         }
     }
